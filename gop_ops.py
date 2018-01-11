40c3af4f0ab2277cb0a6c1c50f91fa6919f5d465
Ming <mingtu@asu.edu>
2018-01-10 17:34:27 -0700

    Initial commit

---
+++ gop_ops.py
@@ -0,0 +1,90 @@
+import syllabifier, sys
+from textgrid import TextGrid
+import numpy as np
+from phn_info import vowels, consonants
+import argparse
+
+def gop_feat(gop_vals, textgrid_file):
+    """
+    Calculate gop statistics on vowels, consonants and syllables
+    :param gop_vals: gop values of one utterance extracted from gop files
+    :param textgrid_file: textgrid file
+    :return:
+    """
+
+    textgrid = TextGrid()
+    textgrid.read(textgrid_file)
+
+    phn_seq = ""
+    for intervals in textgrid.tiers[1]:
+        if intervals.mark is not None:
+            if intervals.mark != 'SIL':
+                phn_seq += " " + intervals.mark
+
+    language = syllabifier.English
+    syllables = syllabifier.syllabify(language, str(phn_seq))
+
+    vowel_gop, consonants_gop, syllable_gop = [], [], []
+
+    syllable_idx = 0  # determine which syllable current phoneme is in
+    phn_idx = 0
+    syllable_phn_gop = []
+    for intervals in textgrid.tiers[1]:
+
+        # vowels and consonants
+        if intervals.mark is not None:
+            if intervals.mark != 'SIL':
+                if intervals.mark in vowels:
+                    vowel_gop.append(gop_vals[phn_idx])
+                elif intervals.mark in consonants:
+                    consonants_gop.append(gop_vals[phn_idx])
+                else:
+                    continue
+
+        # syllables
+        if syllable_idx < len(syllables):
+            current_syllable = syllables[syllable_idx][1] + syllables[syllable_idx][2] + syllables[syllable_idx][3]
+            if intervals.mark is not None:
+                if intervals.mark != 'SIL':
+                    if intervals.mark in current_syllable:
+                        syllable_phn_gop.append(gop_vals[phn_idx])
+                    if intervals.mark == current_syllable[-1]:
+                        syllable_idx += 1
+                        syllable_gop.append(np.mean(syllable_phn_gop))
+                        syllable_phn_gop = []
+
+        if intervals.mark is not None:
+            phn_idx += 1
+
+    return [min(vowel_gop), min(consonants_gop), min(syllable_gop), #minimum gops of vowel, consonant and syllable
+            np.mean(vowel_gop), np.mean(consonants_gop), np.mean(syllable_gop),
+            # average gop of vowels, consonants and syllables
+            np.std(vowel_gop), np.std(consonants_gop), np.std(syllable_gop),
+            # standard deviation of gops of vowels, consonants and syllables
+            np.std(vowel_gop) / np.mean(vowel_gop),
+            np.std(consonants_gop) / np.mean(consonants_gop),
+            np.std(syllable_gop) / np.mean(syllable_gop),  # standard deviation normalized by average
+            ], ["gop_minV","gop_minC","gop_minSyl", "gop_avgV", "gop_avgC", "gop_avgSyl", "gop_stdV", "gop_stdC", "gop_stdSyl", "gop_VacroV", "gop_VacroC", "gop_VacroSyl",
+                ]
+
+# test
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gop_file', help='gop result file')
+    parser.add_argument('--align_dir', help='directory where alignment file are stored')
+
+    args = parser.parse_args()
+
+    with open(args.gop_file,'r') as gop_fid:
+        for eachline in gop_fid:
+
+            uttid = eachline.split()[0] #uttid
+            spkname = uttid.split('_')[0]
+            gop_str = eachline.split()[2:-1] # gop values in string
+            gop_vals = [float(item) for item in gop_str] # to float numebrs
+
+            textgrid_file = args.align_dir + '/' + uttid + '.TextGrid' # path of TextGrid file corresponding to uttid
+
+            feat, descript = gop_feat(gop_vals, textgrid_file)
+
+            print("Done!")
---
+++ rhythmic_ops.py
@@ -0,0 +1,110 @@
+import textgrid, syllabifier, sys
+import numpy as np
+from phn_info import vowels, consonants
+
+def nPVI(durations):
+    """
+    Calculate normalized pairwise variability index
+    :param durations:
+    :return:
+    """
+    if not durations:
+        sys.exit("Empty durations. Exit!")
+    s = []
+    for idx in range(1,len(durations)):
+        s.append(float(durations[idx-1]-durations[idx])/float((durations[idx-1]+durations[idx])/2))
+
+    return 100 / float(len(durations)-1) * np.sum(np.abs(s))
+
+def rPVI(durations):
+    """
+    Calculate raw pairwise variability index
+    :param durations:
+    :return:
+    """
+    if not durations:
+        sys.exit("Empty durations. Exit!")
+    s = []
+    for idx in range(1,len(durations)):
+        s.append(float(durations[idx-1]-durations[idx]))
+
+    return np.sum(np.abs(s)) / (len(durations)-1)
+
+def rhythmic_feat(tg_file, threshold = 0.8):
+    """
+    Return rhythmic features from analysis of alignment file (.textgrid)
+    :param tg_file: textgrid file
+    :param threshold: duration threshold that a phoneme will be considered as bad alignment
+    :return:
+    """
+
+    my_tg_file = textgrid.TextGrid()
+    my_tg_file.read(tg_file)
+    phn_seq = ""
+    for intervals in my_tg_file.tiers[1]:
+        if intervals.mark is not None:
+            if intervals.mark != 'SIL':
+                phn_seq += " " + intervals.mark
+
+    language = syllabifier.English
+    syllables = syllabifier.syllabify(language, str(phn_seq))
+
+    vowel_interval, consonants_interval, syllable_interval, sil_interval = [], [], [], []
+
+    syllable_idx = 0 # determine which syllable current phoneme is in
+    syllable_phn_dur = []
+    for intervals in my_tg_file.tiers[1]:
+
+        # vowels and consonants
+        if intervals.mark is not None:
+            if intervals.mark != 'SIL':
+                if intervals.mark in vowels:
+                    if intervals.maxTime - intervals.minTime < threshold:
+                        vowel_interval.append(float(intervals.maxTime) - float(intervals.minTime))
+                elif intervals.mark in consonants:
+                    if intervals.maxTime - intervals.minTime < threshold:
+                        consonants_interval.append(float(intervals.maxTime) - float(intervals.minTime))
+                else:
+                    continue
+            else:
+                sil_interval.append(float(intervals.maxTime) - float(intervals.minTime))
+
+        #syllables
+        if syllable_idx < len(syllables):
+            current_syllable = syllables[syllable_idx][1] + syllables[syllable_idx][2] + syllables[syllable_idx][3]
+            if intervals.mark is not None:
+                if intervals.mark != 'SIL':
+                    if intervals.mark in current_syllable:
+                        if intervals.maxTime - intervals.minTime < threshold:
+                            syllable_phn_dur.append(float(intervals.maxTime) - float(intervals.minTime))
+                    if intervals.mark == current_syllable[-1]:
+                        syllable_idx += 1
+                        syllable_interval.append(sum(syllable_phn_dur))
+                        syllable_phn_dur = []
+
+    # remove onset and offset silence
+    sil_interval_true = sil_interval[1:-1]
+
+    return [np.mean(vowel_interval), np.mean(consonants_interval), np.mean(syllable_interval), # average duration of vowels, consonants and syllables
+            np.std(vowel_interval), np.std(consonants_interval), np.std(syllable_interval), # standard deviation of vowels, consonants and syllables
+            np.std(vowel_interval)/np.mean(vowel_interval), np.std(consonants_interval)/np.mean(consonants_interval),
+            np.std(syllable_interval)/np.mean(syllable_interval), # standard deviation normalized by average
+            np.sum(vowel_interval)/float(my_tg_file.maxTime-my_tg_file.minTime),
+            np.sum(consonants_interval)/float(my_tg_file.maxTime-my_tg_file.minTime),
+            np.sum(syllable_interval)/float(my_tg_file.maxTime-my_tg_file.minTime), # percentage of duration of vowels, consonants and syllables
+            float(len(syllable_interval))/float(my_tg_file.maxTime-my_tg_file.minTime), # number of syllables per second
+            np.sum(sil_interval_true) / float(float(my_tg_file.maxTime) - float(my_tg_file.minTime) - sil_interval[0]-sil_interval[-1]),
+            np.mean(sil_interval_true), np.std(sil_interval_true), # proportion, average and std of silence duration
+            rPVI(vowel_interval), rPVI(consonants_interval), rPVI(syllable_interval), # raw PVI of vowels, consonants and syllables
+            nPVI(vowel_interval), nPVI(consonants_interval), nPVI(syllable_interval) # normalized PVI of vowels, consonants and syllables
+            ], [ "avgV","avgC","avgSyl", "stdV","stdC","stdSyl", "VacroV","VacroC","VacroSyl",
+              "perV","perC","perSyl","SylPerSec","perSil","avgSil","stdSil",
+              "rPVIV","rPVIC","rPVISyl","nPVIV","nPVIC","nPVISyl"
+            ]
+
+# testing
+if __name__ == "__main__":
+
+    feats, descrip = rhythmic_feat('/home/ming/Work_Ming/Dissertation/exp_folder/feat_extract/english/align_feat/aligned_textgrid/english32.TextGrid')
+
+    print ("Done")
