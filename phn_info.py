31e0b1924a1bcf99ca375fd81f35c18ef2e9741b
Ming <mingtu@asu.edu>
2018-03-30 13:05:27 -0700

    Add phonemes sets

---
+++ phn_info.py
@@ -0,0 +1,17 @@
+# Change the vowels and consonants set based on your own dataset
+
+# vowels = ['IY','EH','AA','AE','AH','AO','IH','EY','AW','AY','ER','UW','OW']
+# consonants = ['DH','B','JH','NG','CH','D','G','F','K','M','N','S','R','T','V','Z','P','SH']
+# front_vowels = ['IY','IH','AE','EY','AY']
+# middle_vowels = ['AH','ER']
+# back_vowels = ['AA','AO','AW','UW','OW']
+# bilabial = ['M','B','P']
+# labio_dental = ['F','V']
+# lingua_dental = ['DH']
+# lingua_alveolar = ['T','D','S','Z','CH','JH','N']
+# lingual_palatal = ['SH','R']
+# lingua_velar = ['K','G','NG']
+# #glottal = ['H']
+
+vowels = ['IY','AA','AE','EH','AH','AO','IH','EY','AW','AY','ER','UW','UH','OY','OW']
+consonants = ['W','DH','Y','HH','CH','JH','TH','ZH','D','NG','B','G','F','K','M','L','N','S','R','T','V','Z','P','SH']
